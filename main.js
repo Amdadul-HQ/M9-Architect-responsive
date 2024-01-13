c99796eb9b1dcf82b33221eee99c0d3db630d042
AmdadulHQ <rimonamdadul301@gmail.com>
2024-01-14 00:33:35 +0600

    add responsive navbar

---
+++ main.js
@@ -0,0 +1,8 @@
+function showSidevar () {
+    const sideshow = document.querySelector('.sidevar');
+    sideshow.style.display = 'flex'
+}
+function closevar (){
+    const closeside = document.querySelector('.sidevar');
+    closeside.style.display = 'none'
+}
